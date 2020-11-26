be573de969676f8af3a6b3b72522926deff2f05b
AndreasChristianson <33329956+AndreasChristianson@users.noreply.github.com>
2020-11-25 20:12:20 -0600

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'devicon/fonts/devicon.eot'
+import 'fontsource-merriweather'
+import 'fontsource-open-sans'
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+import { Homepage } from './pages/Homepage';
+
+ReactDOM.render(
+  <Homepage />,
+  document.getElementById('root')
+);
