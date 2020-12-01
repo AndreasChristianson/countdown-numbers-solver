5b880f9935eaa5b5625e0acbe58e6b5254b0ec66
Andreas Christianson <sullage@pessimistic-it.com>
2020-11-30 21:50:55 -0600

    some solution section styling

---
+++ src/components/Step.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StepContainer = styled.pre``
+
+export const Step = ({ step }) => (
+  <StepContainer>
+    {step}
+  </StepContainer>
+)
--- src/sections/Solutions.jsx
+++ src/sections/Solutions.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Solution } from '../components/Solution'
 import { findSolutions } from '../solutions/find-solutions-v4'
 
 export const Solutions = ({ numbers, target, operations }) => {
   const solutions = findSolutions({ numbers, target, operations })
-  return <div>{JSON.stringify(solutions)}</div>
+  return <div>
+    {solutions.map((solution, index) => <Solution solution={solution} key={index}/>)}
+  </div>
 }
 
